e6646c615640ca264824bb79f6edbea64869e05f
Gleb <glsidorenko01@gmail.com>
2022-05-24 14:35:10 +0300

    fix: the status of the files-copy folder has not been updated

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,26 @@
 const fileStream = require('fs');
+const promises = fileStream.promises;
 const path = require('path');
-
 const pathToCopy = path.join(__dirname, '/files');
 const pathDestination = path.join(__dirname, '/files-copy');
 
+async function startCopy(from, to) {
+  await promises.rm(to, {force: true, recursive: true });
+  await promises.mkdir(to, { recursive: true });
 
-fileStream.mkdir(pathDestination, { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fileStream.readdir(pathToCopy, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach(file => {
-      const pathToFile = path.join(pathToCopy, file);
-      fileStream.copyFile(pathToFile, path.join(pathDestination, file), () => {
-        console.log(`${file} was copied to files-copy`);
+  fileStream.readdir(from, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        const pathToFile = path.join(from, file);
+        fileStream.copyFile(pathToFile, path.join(to, file), () => {
+          console.log(`${file} was copied to files-copy`);
+        });
       });
-    });
-  }
-});
+    }
+  });
+}
+
+startCopy(pathToCopy, pathDestination);
+
